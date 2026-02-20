a07c5417ade044d85adbf0a369cdb1e15daa18fe
BrettMyburgh <myburghb@gmail.com>
2026-02-20 17:32:05 +0200

    Started with the sass

--- beansBunnies/settings.py
+++ beansBunnies/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
